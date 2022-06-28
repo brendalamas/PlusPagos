5ad024988bfa49068a71b99327184f9a756a88da
brendalamas <lamasbrenda@hotmail.com>
2022-06-28 16:53:08 -0300

    organice carpetas

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const burger = document.querySelector(".burger");
 const accesos = document.querySelector(".header-accesos");
 const menuBurger = document.querySelector(".menu-burger");
+const botonDescargar = document.querySelector("#boton-descargar");
 
 
 burger.onclick = () =>{
-    menuBurger.classList.toggle('display')
+    menuBurger.classList.toggle('display-flex')
+}
+
+botonDescargar.onclick = () =>{
+    seccionFacturasDigitales.classList.toggle('display-flex')
+    console.log("click en descargar");
 }
