a28cd57f0a32e9b0bbb29c9fe9c59357093c62d2
brendalamas <lamasbrenda@hotmail.com>
2022-07-01 17:22:08 -0300

    saque html form y puse en js

--- index.js
+++ index.js
@@ -3,13 +3,57 @@ const accesos = document.querySelector(".header-accesos");
 const menuBurger = document.querySelector(".menu-burger");
 const botonDescargar = document.querySelector("#boton-descargar");
 
+const header = document.querySelector("header")
+const seccionCarrusel = document.querySelector(".seccion-carrusel")
+const seccionBox = document.querySelector(".seccion-box")
+const seccionImpuestos = document.querySelector(".seccion-impuestos")
+const seccionSucursales = document.querySelector(".seccion-sucursales")
+const seccionProductos = document.querySelector(".seccion-productos")
+const contactanos = document.querySelector("#contactanos")
+const footer = document.querySelector("footer")
+const seccionFacturas = document.querySelector(".seccion-facturas")
+
+const botonBuscar = document.querySelector ("#buscar")
+const modal = document.querySelector(".modal")
+const cerrarModal = document.querySelector(".cerrar-modal")
+
+const botonAtras = document.querySelector(".boton-atras")
+
 
 burger.onclick = () =>{
     menuBurger.classList.toggle('display-flex')
     console.log("click burger");
 }
 
-botonDescargar.onclick = () =>{
-    seccionFacturasDigitales.classList.toggle('display-flex')
+// seccion facturas 
+botonDescargar.onclick=()=>{
+    header.style.display = "none"
+    seccionCarrusel.style.display = "none"
+    seccionBox.style.display = "none"
+    seccionImpuestos.style.display = "none"
+    seccionSucursales.style.display = "none"
+    seccionProductos.style.display = "none"
+    contactanos.style.display = "none"
+    footer.style.display = "none"
+    seccionFacturas.style.display = "flex"
     console.log("click en descargar");
+}
+// seccion facturas modal
+botonBuscar.onclick = () =>{
+    modal.style.display="flex"
+}
+cerrarModal.onclick = () =>{
+    modal.style.display="none"
+}
+botonAtras.onclick = () => {
+    header.style.display = "flex"
+    seccionCarrusel.style.display = "flex"
+    seccionBox.style.display = "flex"
+    seccionImpuestos.style.display = "flex"
+    seccionSucursales.style.display = "flex"
+    seccionProductos.style.display = "flex"
+    contactanos.style.display = "flex"
+    footer.style.display = "flex"
+    seccionFacturas.style.display = "none"
+    console.log("click en atras");
 }
